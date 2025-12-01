5cbbb345c58f8b1ce5842179f3ce238a084ffd0a
salmaseffen <160020302+salmaseffen@users.noreply.github.com>
2025-12-02 00:59:32 +0100

    Update main.dart
    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'providers/country_provider.dart';
 import 'providers/theme_provider.dart';
 import 'screens/home_screen.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
