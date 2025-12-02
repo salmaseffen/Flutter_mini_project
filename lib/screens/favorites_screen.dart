b9f36650be2cf98c026785e8cbe0cf59aebedd3c
salmaseffen <160020302+salmaseffen@users.noreply.github.com>
2025-12-02 01:01:22 +0100

    Update favorites_screen.dart
    Set BottomNavigationBar

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/country_provider.dart';
 import '../widgets/country_card.dart';
+
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({super.key});
 
